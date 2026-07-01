e06234be038a19cce95107aaf7e1e3c1303bdf3d
komal0a <komal22124@gmail.com>
2026-07-01 21:13:49 +0530

    Fix CORS for Vercel deployment

--- api/src/main.ts
+++ api/src/main.ts
@@ -8,10 +8,13 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: 'http://localhost:5173',
-    credentials: true,
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
-  });
+  origin: [
+    'http://localhost:5173',
+    'https://weather-guard-eight.vercel.app',
+  ],
+  credentials: true,
+  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+});
 
   app.useGlobalPipes(
   new ValidationPipe({
