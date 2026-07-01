f80d13a7657e89642ef565d047ea5b69a5c5311f
komal0a <komal22124@gmail.com>
2026-07-01 21:22:52 +0530

    changing main.ts

--- api/src/main.ts
+++ api/src/main.ts
@@ -9,14 +9,9 @@ async function bootstrap() {
 
   console.log("FRONTEND_URL =", process.env.FRONTEND_URL);
   app.enableCors({
-  origin: [
-    'http://localhost:5173',
-    'https://weather-guard-eight.vercel.app',
-  ],
+  origin: process.env.FRONTEND_URL || "http://localhost:5173",
   credentials: true,
-  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
 });
-
   app.useGlobalPipes(
   new ValidationPipe({
     whitelist: true,
